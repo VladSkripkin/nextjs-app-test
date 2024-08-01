ee15f80fd68b55104ff93564cb2ffd85a9f03bd1
Vladislav Skripkin <vladislav.skripkin@marakas.digital>
2024-08-01 09:44:23 +0300

    fix dark theme, fix scroll

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -11,9 +11,9 @@ export default function Hero() {
                     In today's dynamic digital landscape, efficient security operations are crucial for organizational success and resilience. Cutting-edge automation transforms compliance governance and risk assessment.
                 </div>
                 <div className="hero-bottom flex flex-col justify-center items-center gap-3 mt-10">
-                    <button type="button" className="btn flex justify-center items-center h-14">
+                    <a className="btn flex justify-center items-center h-14" href="#contact">
                         Get started
-                    </button>
+                    </a>
                     <div className="hero-since flex flex-row justify-center items-center gap-2">
                         <p>Pushing the limits since 2013</p>
                         <Image src="/icon-since.svg" alt="since" width={18} height={14}/>
--- src/app/components/slogan.tsx
+++ src/app/components/slogan.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 export default function Slogan() {
     return(
-        <section className="slogan relative h-dvh" id="culture">
+        <section className="slogan relative h-dvh scroll-pt-10" id="culture" >
             <div className="slogan-decor"></div>
             <div className="container h-full flex flex-col items-center justify-center relative">
                 <div className="title-m slogan-text">
